af35d875e29b59119c58c79ea1e17db673f2d718
c3 <alphawing3000@gmail.com>
2025-04-08 11:38:19 -0400

    Fixed typing animation and cursor position

--- cursoranimation.js
+++ cursoranimation.js
@@ -1,13 +1,14 @@
+const container = document.querySelector('#header-container');
 const cursor = document.createElement('span');
 cursor.textContent = '|';
 cursor.id = 'cursor';
 
-const header = document.querySelector('header');
 const h1 = document.createElement('h1');
 const span = document.createElement('span');
 const p = document.createElement('p');
+const br = document.createElement('br');
 
-let h1Text = "A Portolio,"
+let h1Text = "A Portfolio, "
 let spanText = "by C3"
 let pText = "Welcome to my Portfolio."
 
@@ -20,10 +21,35 @@ let headerElements = new Map([
 ]);
 
 
-function typeText(element, text) {
+container.appendChild(h1);
+container.appendChild(span);
+container.appendChild(br);
+container.appendChild(p);
+
+h1.classList.add("header-text");
+span.classList.add("header-text");
+p.classList.add("header-text");
+
+
+document.addEventListener ('keydown', () => {
+  animating = false;
+});
+
+function moveCursor(element) { //places the cursor after an element
+  cursor.remove();
+  cursor.style.fontSize = getComputedStyle(element).fontSize;
+  element.insertAdjacentElement('afterend', cursor);
+}
+
+function typeText(map) {
 
   let i = 0;
+  let j = 0;
   let speed = 100;
+
+  let element = Array.from(map.entries())[j][0];
+  let text = Array.from(map.entries())[j][1];
+  moveCursor(element)
  
   function type(){
     if (i < text.length && animating) {
@@ -31,29 +57,21 @@ function typeText(element, text) {
       i++;
       setTimeout(type, speed);
 
-    }else{
+    }else {
       element.textContent = text;
+      if (j < Array.from(map.entries()).length - 1){
+        j++;
+        i = 0;
+        element = Array.from(map.entries())[j][0];
+        text = Array.from(map.entries())[j][1];
+        moveCursor(element);
+        setTimeout(type, speed);
+      }
     }
   }
-    type();
-}
 
-function moveCursor(element) { //places the cursor after an element
-  cursor.remove();
-  element.insertAdjacentElement('afterend', cursor);
+  type();
 }
 
 
-document.addEventListener ('keydown', () => {
-  animating = false;
-});
-
-header.appendChild(h1);
-h1.appendChild(span);
-header.appendChild(p);
-
-for (const [element, text] of headerElements.entries()) {
-  moveCursor(element);
-  typeText(element, text);
-  
-}
+typeText(headerElements);
